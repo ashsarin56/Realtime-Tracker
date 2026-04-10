5b9ef55b72089a44bf89713f55e54cd7507627ae
ashsarin56 <arinshukla4@gmail.com>
2026-04-10 18:55:44 +0530

    map integrated

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const app = express();
+const path=require('path');
 const http = require('http');
 const socketio = require('socket.io');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-const path = require('path');
 dotenv.config();
+
 app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'));
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 3000;
 
 const server=http.createServer(app);
 const io=socketio(server);
-app.get('/', (req, res) => {
-    res.send('hello there!');
+io.on('connection', (socket) => {
+    console.log('connection established');
 });
-app.listen(PORT, () => {
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
 });
